9308004c9952789f636e161845efd5678fe406e1
Joel Mena <joelmena56@hotmail.com>
2019-12-19 01:14:31 -0400

    ya estoy cargando datos desde la DB de forma manual en la vista Reporte

--- Controllers/ReportesController.cs
+++ Controllers/ReportesController.cs
@@ -3,20 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using XMedicalLite.Models;
+using XMedicalLite_Windows.Models;
 
 namespace XMedicalLite_Windows.Controllers
 {
     public class ReportesController : Controller
     {
+        MyDbContext db = new MyDbContext();
         // GET: Reportes
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult Reporte()
+        public ActionResult Reporte(int id)
         {
-            return View();
+            Reporte reporte = new Reporte();
+            reporte.Expediente = db.Expedientes.Find(id);
+            reporte.Paciente = db.Pacientes.Find(reporte.Expediente.PacienteID);
+            return View(reporte);
         }
     }
 }
---
+++ Models/Reporte.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using XMedicalLite.Models;
+
+namespace XMedicalLite_Windows.Models
+{
+    public class Reporte
+    {
+        public Paciente Paciente { get; set; }
+        public Expediente Expediente { get; set; }
+    }
+}
